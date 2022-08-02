87832cc54489bf90cde21de8321623b42cf002be
Alesouza7 <alexcostasouza86@yahoo.com>
2022-08-01 23:08:03 -0300

    atualização

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,7 @@ const filme = {
 };
 
 // respostas aqui ⬇️
+console.log("a)O nome do primeiro ator:",filme.elenco[0])
+console.log("b)O nome do ultimo ator:",filme.elenco[3])
+console.log("c)O nome de todos os atores:",filme.elenco)
+console.log("d)O horário da transmissão do filme:",filme.transmissoesHoje[1])
