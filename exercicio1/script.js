2e2cd5bd894c5306ac4fbb7d529f5a5c3e3af473
Alesouza7 <alexcostasouza86@yahoo.com>
2022-08-01 22:46:57 -0300

    Exercicios concluidos

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+//1ª parte 
+const sacolao = []
+const frutasacolaofrutas = {
+        nome:"banana",
+        preco:4.99,
+        disponibilidade:true
+    }
+sacolao.push(frutasacolaofrutas)
+console.log(sacolao)
+
